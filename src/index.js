3c4baebb30c1d160b4d5c8aaac0b6d48acedc0c1
Jaja <jestrella@pacificseainvests.com>
2024-11-19 08:25:17 +0800

    create the star rating component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating="10"/>
+    <StarRating 
+      color='red'
+      size={30}
+      messages={['Terrible', 'Poor', 'Okay', 'Good', 'Satisfied', 'Very Good', 'Excellent', 'Outstanding', 'Exceptional', 'Perfect']}
+      className=""
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
 
